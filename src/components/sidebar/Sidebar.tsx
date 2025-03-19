129954b53fd52f100a6d1bd00b734b674a20cce4
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-03-19 21:31:25 +0100

    Style sidebar: Updated UI for Gemini clone

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,47 @@
+import { useState } from 'react';
+import { assets } from '../../assets/assets';
 import './Sidebar.css';
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  const [extended, setExtended] = useState(false);
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img
+          onClick={() => setExtended(!extended)}
+          src={assets.menu_icon}
+          alt="menu"
+          className="menu"
+        />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="plus_icon" />
+          {extended && <p>New Chat</p>}
+        </div>
+        {extended && (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message_icon} alt="message_icon" />
+              <p>What is Typescript...</p>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="question_icon" />
+          {extended && <p>Help</p>}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="history_icon" />
+          {extended && <p>Activity</p>}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="setting_icon" />
+          {extended && <p>Settings</p>}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
