bc056ad79c9b88c20f8af271e7685a600744fd16
Josaphat JM <josaphatmurhabazi2016@gmail.com>
2025-03-26 21:06:36 +0100

    Integrate gemini API

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -9,30 +9,31 @@ const Main = () => {
         <img src={assets.user_icon} alt="user_icon" />
       </div>
       <div className="main-container">
-        <div className="greet">
-          <p>
-            <span>Hello, Josaphat</span>
-            <p>How can I help you today?</p>
-          </p>
-        </div>
-        {/* <div className="cards">
-          <div className="card">
-            <p>Suggest beautiful places to see on a upcoming road trip</p>
-            <img src={assets.compass_icon} alt="compass_icon" />
+        <>
+          <div className="greet">
+            <p>
+              <span>Hello, Josaphat</span>
+            </p>
           </div>
-          <div className="card">
-            <p>Briefly summarize this concept:urban planning</p>
-            <img src={assets.bulb_icon} alt="compass_icon" />
+          <div className="cards">
+            <div className="card">
+              <p>Suggest beautiful places to see on a upcoming road trip</p>
+              <img src={assets.compass_icon} alt="compass_icon" />
+            </div>
+            <div className="card">
+              <p>Briefly summarize this concept:urban planning</p>
+              <img src={assets.bulb_icon} alt="compass_icon" />
+            </div>
+            <div className="card">
+              <p>Brainstorm team bonding activities for for our work retreat</p>
+              <img src={assets.message_icon} alt="compass_icon" />
+            </div>
+            <div className="card">
+              <p>Improve the readability of the follwing code</p>
+              <img src={assets.code_icon} alt="compass_icon" />
+            </div>
           </div>
-          <div className="card">
-            <p>Brainstorm team bonding activities for for our work retreat</p>
-            <img src={assets.message_icon} alt="compass_icon" />
-          </div>
-          <div className="card">
-            <p>Improve the readability of the follwing code</p>
-            <img src={assets.code_icon} alt="compass_icon" />
-          </div>
-        </div> */}
+        </>
 
         <div className="main-bottom">
           <div className="search-box">
